527bb0911289f1935f1c75dcff6362e1ef0e3822
Youssef filali <filaliy71@gmail.com>
2024-12-17 00:19:37 +0100

    i add an animation

--- app/components/HomePageBanner.jsx
+++ app/components/HomePageBanner.jsx
@@ -79,11 +79,11 @@ function HomePageBanner() {
             </div>
           </div>
         </motion.div>
-        <motion.div variants={itemVariants} initial="hidden" animate="visible" className="md:mt-0 md:mb-20">
+        <motion.div variants={itemVariants} initial="hidden" animate="visible" className="md:mt-0 m d:mb-20">
           <Image src="/code-typing-animate.svg" width={384} height={384} alt="coding pic" className="drop-shadow-lg w-96" priority layout="intrinsic" />{" "}
         </motion.div>
       </div>
-      <div className="flex justify-center max-md:py-12" variants={itemVariants} initial="hidden" animate="visible">
+      <motion.div className="flex justify-center max-md:py-12" variants={itemVariants} initial="hidden" animate="visible">
         <div className="inline-flex w-3/5 max-md:w-full flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
           <ul className="flex animate-infinite-scroll items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8">
             {techLogos.map((logo, index) => (
@@ -95,12 +95,12 @@ function HomePageBanner() {
           <ul className="flex animate-infinite-scroll items-center justify-center md:justify-start [&_img]:max-w-none [&_li]:mx-8" aria-hidden="true">
             {techLogos.map((logo, index) => (
               <li key={index}>
-                <Image src={logo.src} alt={logo.alt} width={50} height={50} className="rounded-md max-md:w-12 max-md:h-12" priority loading="lazy" />
+                <Image src={logo.src} alt={logo.alt} width={50} height={50} className="rounded-md max-md:w-12 max-md:h-12" loading="lazy" />
               </li>
             ))}
           </ul>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
